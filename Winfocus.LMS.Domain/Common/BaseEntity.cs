97b34937130b28b5cb76c1f17b4122ab226ed0ea
Winfocus-LMS <Karthikthekkedath@outlook.com>
2026-02-02 19:48:45 +0530

    Add BaseEntity and placeholders; clean up project structure
    Removed explicit folder includes from Domain csproj and added Data folder to Infrastructure csproj. Deleted Visual Studio settings file. Introduced BaseEntity abstract class with common properties. Added placeholder classes for Centre, Country, and CentreType.

---
+++ Winfocus.LMS.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winfocus.LMS.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
