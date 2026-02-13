eac3e24cf0f54a63835f3ab2bb0af4c536770183
sruthyar1999@gmail.com <sruthyar1999@gmail.com>
2026-02-13 09:35:32 +0530

    academic details

---
+++ Winfocus.LMS.Application/Interfaces/IStudentAcademicdeatilsRepository.cs
@@ -0,0 +1,44 @@
+﻿namespace Winfocus.LMS.Application.Interfaces
+{
+    using Winfocus.LMS.Domain.Entities;
+
+    /// <summary>
+    /// Defines CRUD operations for <see cref="StudentAcademicDetails"/> entities.
+    /// </summary>
+    public interface IStudentAcademicdeatilsRepository
+    {
+        /// <summary>
+        /// Gets all asynchronous.
+        /// </summary>
+        /// <returns>StudentAcademicDetails.</returns>
+        Task<IReadOnlyList<StudentAcademicDetails>> GetAllAsync();
+
+        /// <summary>
+        /// Gets the by identifier asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>StudentAcademicDetails.</returns>
+        Task<StudentAcademicDetails?> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Adds the asynchronous.
+        /// </summary>
+        /// <param name="studentAcademicDetails">The StudentAcademicDetails.</param>
+        /// <returns>studentAcademicDetails.</returns>
+        Task<StudentAcademicDetails> AddAsync(StudentAcademicDetails studentAcademicDetails);
+
+        /// <summary>
+        /// Updates the asynchronous.
+        /// </summary>
+        /// <param name="studentAcademicDetails">The state.</param>
+        /// <returns>studentAcademicDetails.</returns>
+        Task UpdateAsync(StudentAcademicDetails studentAcademicDetails);
+
+        /// <summary>
+        /// Deletes the asynchronous.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>task.</returns>
+        Task DeleteAsync(Guid id);
+    }
+}
