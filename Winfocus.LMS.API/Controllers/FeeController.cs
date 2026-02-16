ea3ed8b13d73098e43dce65afddb5c02c7cc501d
Winfocus-LMS <Karthikthekkedath@outlook.com>
2026-02-16 16:36:14 +0530

    Added Fee entities and endpoints and added the country seeder file with operating country list

---
+++ Winfocus.LMS.API/Controllers/FeeController.cs
@@ -0,0 +1,52 @@
+﻿namespace Winfocus.LMS.API.Controllers
+{
+    using Asp.Versioning;
+    using Microsoft.AspNetCore.Mvc;
+    using Winfocus.LMS.Application.DTOs.Fees;
+    using Winfocus.LMS.Application.Interfaces;
+
+    /// <summary>
+    /// FeeController.
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public sealed class FeeController : ControllerBase
+    {
+        private readonly IFeeService _service;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeeController"/> class.
+        /// </summary>
+        /// <param name="service">The service.</param>
+        public FeeController(IFeeService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Gets the fee page.
+        /// </summary>
+        /// <param name="studentId">The student identifier.</param>
+        /// <returns>Task&lt;IActionResult&gt;.</returns>
+        [HttpGet("{studentId}")]
+        public async Task<IActionResult> GetFeePage(Guid studentId)
+        {
+            var result = await _service.GetFeePageAsync(studentId);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Selects the fee.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns>Task&lt;IActionResult&gt;.</returns>
+        [HttpPost("select")]
+        public async Task<IActionResult> SelectFee(SelectFeeRequestDto request)
+        {
+            var result = await _service.SelectFeeAsync(request);
+            return Ok(result);
+        }
+    }
+}
---
+++ Winfocus.LMS.Domain/Entities/FeePlan.cs
@@ -0,0 +1,84 @@
+﻿namespace Winfocus.LMS.Domain.Entities
+{
+    using Winfocus.LMS.Domain.Common;
+
+    /// <summary>
+    /// FeePlan.
+    /// </summary>
+    /// <seealso cref="Winfocus.LMS.Domain.Common.BaseEntity" />
+    public sealed class FeePlan : BaseEntity
+    {
+        /// <summary>
+        /// Gets the course identifier.
+        /// </summary>
+        /// <value>
+        /// The course identifier.
+        /// </value>
+        public Guid CourseId { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the plan.
+        /// </summary>
+        /// <value>
+        /// The name of the plan.
+        /// </value>
+        public string PlanName { get; private set; } = null!;
+
+        /// <summary>
+        /// Gets the tuition fee.
+        /// </summary>
+        /// <value>
+        /// The tuition fee.
+        /// </value>
+        public decimal TuitionFee { get; private set; }
+
+        /// <summary>
+        /// Gets the registration fee.
+        /// </summary>
+        /// <value>
+        /// The registration fee.
+        /// </value>
+        public decimal RegistrationFee { get; private set; }
+
+        /// <summary>
+        /// Gets the scholarship percent.
+        /// </summary>
+        /// <value>
+        /// The scholarship percent.
+        /// </value>
+        public decimal ScholarshipPercent { get; private set; }
+
+        /// <summary>
+        /// Gets the seasonal percent.
+        /// </summary>
+        /// <value>
+        /// The seasonal percent.
+        /// </value>
+        public decimal SeasonalPercent { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is installment allowed.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is installment allowed; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsInstallmentAllowed { get; private set; }
+
+        /// <summary>
+        /// Gets the course.
+        /// </summary>
+        /// <value>
+        /// The course.
+        /// </value>
+        public Course Course { get; private set; } = null!;
+
+        /// <summary>
+        /// Gets the installments.
+        /// </summary>
+        /// <value>
+        /// The installments.
+        /// </value>
+        public ICollection<FeeInstallment> Installments { get; private set; }
+            = new List<FeeInstallment>();
+    }
+}
