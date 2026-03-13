481ce7b0eed30d22d981f907e5ed0ff6699342f6
sruthyar1999@gmail.com <sruthyar1999@gmail.com>
2026-03-13 12:41:18 +0530

    335 - Editable Page Headings API and get, getall apis

---
+++ Winfocus.LMS.Application/DTOs/UpdatePageHeadingDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Winfocus.LMS.Application.DTOs
+{
+    /// <summary>
+    /// Represents the request model used to update the heading
+    /// information of a specific page.
+    /// </summary>
+    public class UpdatePageHeadingDto
+    {
+        /// <summary>
+        /// Gets or sets the main heading displayed at the top of the page.
+        /// </summary>
+        public string MainHeading { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the sub heading displayed below the main heading.
+        /// </summary>
+        public string SubHeading { get; set; } = string.Empty;
+    }
+}
---
+++ Winfocus.LMS.Application/Interfaces/IPageHeadingRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Winfocus.LMS.Domain.Entities;
+
+namespace Winfocus.LMS.Application.Interfaces
+{
+    /// <summary>
+    /// Repository contract for managing <see cref="PageHeading"/> entities.
+    /// Provides data access methods for retrieving and updating
+    /// page heading information used in the admin interface.
+    /// </summary>
+    public interface IPageHeadingRepository
+    {
+        /// <summary>
+        /// Retrieves all page headings from the data store.
+        /// </summary>
+        /// <returns>
+        /// A list of <see cref="PageHeading"/> entities.
+        /// </returns>
+        Task<List<PageHeading>> GetAllAsync();
+
+        /// <summary>
+        /// Retrieves a specific page heading by its unique page key.
+        /// </summary>
+        /// <param name="pageKey">
+        /// The unique identifier representing the page.
+        /// </param>
+        /// <returns>
+        /// The matching <see cref="PageHeading"/> entity if found; otherwise <c>null</c>.
+        /// </returns>
+        Task<PageHeading?> GetByPageKeyAsync(string pageKey);
+
+        /// <summary>
+        /// Updates an existing page heading in the data store.
+        /// </summary>
+        /// <param name="pageHeading">
+        /// The <see cref="PageHeading"/> entity containing updated heading information.
+        /// </param>
+        /// <returns>
+        /// A task representing the asynchronous update operation.
+        /// </returns>
+        Task<PageHeading> UpdateAsync(PageHeading pageHeading);
+    }
+}
