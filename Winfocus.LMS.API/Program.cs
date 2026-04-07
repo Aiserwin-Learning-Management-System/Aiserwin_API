056da3a0f44d6c543ea2eb760266b39304abf66b
Abidviera <zainulzain043@gmail.com>
2026-04-07 11:08:17 +0530

    feat: initialize API entry point and configure dependency injection for application services

--- Winfocus.LMS.API/Program.cs
+++ Winfocus.LMS.API/Program.cs
@@ -67,8 +67,7 @@ builder.Services.AddCors(options =>
 
         policy.WithOrigins(allowedOrigins)
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
